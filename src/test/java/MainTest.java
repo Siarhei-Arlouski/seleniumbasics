3357360f3893c030fbff18ba2e416ea419111fb8
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-12-11 14:59:32 +0300

    Added TestNG dependency

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class MainTest {
 
     WebDriver driver = new ChromeDriver();
 
+    @Test
     void firstLink() {
         driver.get("https://the-internet.herokuapp.com/");
     }
