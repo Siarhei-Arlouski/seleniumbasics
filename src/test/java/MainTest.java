04255d9af95448ab50f6aba067a66dcd10f14fa7
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-12-11 15:25:25 +0300

    Added list of links and clicking on the tenth link

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,14 +1,20 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class MainTest {
 
     WebDriver driver = new ChromeDriver();
 
     @Test
-    void firstLink() {
+    void objLinks() {
         driver.get("https://the-internet.herokuapp.com/");
+        List<WebElement> objLinks = driver.findElements(By.tagName("a"));
+        objLinks.get(10).click();
     }
 
 }
