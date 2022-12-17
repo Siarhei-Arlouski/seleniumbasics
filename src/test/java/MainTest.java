870572a32c4e09b1b740d875eccc9d7789115ab6
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-12-17 18:56:19 +0300

    Added double click and drag and drop tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,10 +10,9 @@ import java.util.List;
 
 public class MainTest {
 
-    WebDriver driver = new ChromeDriver();
-
     @Test
     void objLinks() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         List<WebElement> objLinks = driver.findElements(By.tagName("a"));
         objLinks.get(10).click();
@@ -20,6 +20,7 @@ public class MainTest {
 
     @Test
     void homePage() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://litecart.stqa.ru/en/");
         WebElement homePageLink = driver.findElement(By.xpath("//i[@title='Home']"));
         homePageLink.click();
@@ -31,6 +32,7 @@ public class MainTest {
 
     @Test
     void rubberDucksPage() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://litecart.stqa.ru/en/");
         WebElement rubberDucksPageLink = driver.findElement(By.xpath("//nav[@id='site-menu']//a[text()='Rubber Ducks']"));
         rubberDucksPageLink.click();
@@ -42,6 +44,7 @@ public class MainTest {
 
     @Test
     void deliveryInformationPage() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://litecart.stqa.ru/en/");
         WebElement deliveryInformationPageLink = driver.findElement(By.xpath("//*[text()='Delivery Information']"));
         deliveryInformationPageLink.click();
@@ -53,6 +56,7 @@ public class MainTest {
 
     @Test
     void termsAndConditions() {
+        WebDriver driver = new ChromeDriver();
         driver.get("https://litecart.stqa.ru/en/");
         WebElement termsAndConditionsLink = driver.findElement(By.xpath("//*[text()='Terms & Conditions']"));
         termsAndConditionsLink.click();
@@ -61,6 +65,38 @@ public class MainTest {
 
         Assert.assertEquals(titleTACPage, "Terms & Conditions | My Store");
 
+    }
+
+    @Test
+    void iconDoubleClick() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.pbclibrary.org/mousing/click3.htm");
+
+        WebElement IEIcon = driver.findElement(By.name("icon1"));
+        WebElement WordIcon = driver.findElement(By.name("icon2"));
+        WebElement PPIcon = driver.findElement(By.name("icon3"));
+
+        Actions builder = new Actions(driver);
+
+        builder.doubleClick(IEIcon).doubleClick(WordIcon).doubleClick(PPIcon).perform();
+    }
+
+    @Test
+    void ballDragAndDrop() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://learn.javascript.ru/article/mouse-drag-and-drop/ball4/");
+
+        WebElement ball = driver.findElement(By.id("ball"));
+        WebElement gate = driver.findElement(By.id("gate"));
+
+        Actions builder = new Actions(driver);
+
+        builder.dragAndDrop(ball, gate).perform();
+    }
+
+    @Test
+    void alerts() {
+        
     }
 
 }
